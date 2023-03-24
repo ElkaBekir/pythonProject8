2f2da252f64e4417b2782403453717013e09dea4
Mr.Eldar <elka.bekir@gmail.com>
2023-03-24 16:14:32 -0400

    lesson

---
+++ lessonday8/in_range.py
@@ -0,0 +1,21 @@
+#for x in range(1,10):
+    #print(x)
+   # print(x*x)
+
+    #for letter in "Uncle genius":
+        #print(letter*10)
+
+range(10)
+r = range(10)
+
+list(r)
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+nums = range(1, 10, 2)
+
+
+list(nums)
+quit()
+
+for num in range(0, 100, 5):
+    print(num)
---
+++ lessonday8/times.py
@@ -0,0 +1,9 @@
+times = input("How many times do I have to tell you?")
+times = int(times)
+
+#for time in range(times):
+    #print("CLEAN UP YOUR ROOM")
+
+for time in range(times):
+    print(f"time {time+1}:CLEAN UP YOUR ROOM")
+
